472d8eb4b0c8d01b5eeb240a390484668e25e81f
Kritika Srivastava <kritikasri27@gmail.com>
2023-01-12 01:22:34 +0530

    Added cart icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,33 +16,19 @@ export const Navbar = () => {
             <li>Products</li>
           </Link>
         </ul>
-
-        {/* <div className="user">
-          {isUser ? (
-            <button
-              className="login"
-              onClick={() => logout({ returnTo: window.location.origin })}
-            >
-              Log out
-            </button>
-          ) : (
-            <button className="login" onClick={loginWithRedirect}>
-              Log in
-            </button>
-          )}
-          <Link to="/cart">
+        <div className="user">
+        <Link to="/cart">
             <i className="fa fa-shopping-cart fa-2x cart-icon"></i>{" "}
           </Link>
-          <div className="nav-item">
+        <div className="nav-item">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path d="M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z" />
             </svg>
             <div className="amount-container">
-              <p className="total-amount">{amount}</p>
+              <p className="total-amount">0</p>
             </div>
           </div>
         </div>
-      </div> */}
     </div>
     </div>
   );
