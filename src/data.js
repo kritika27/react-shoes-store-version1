225fd84a269e404f2d7103b7db8a7d1ada334153
Kritika Srivastava <kritikasri27@gmail.com>
2023-01-12 01:22:08 +0530

    Added size and colors to store data

--- src/data.js
+++ src/data.js
@@ -2,65 +2,79 @@ const data= [{
     id: 10,
     name: 'Adidas NMD',
     imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-    // imageUrl: "/images/shop-img/sneakers/adidas-nmd.png",
     price: 220,
-    colors:['red','green','blue']
+    amount:1,
+    colors:['red','green','blue'],
+    size:['5','6']
 },
 {
     id: 11,
     name: 'Adidas Yeezy',
     imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-    // imageUrl: "/images/shop-img/sneakers/yeezy.png",
     price: 280,
-    colors:['red','green','blue']
+    amount:1,
+    colors:['red','green','blue'],
+    size:['5','6']
 },
 {
     id: 12,
     name: 'Black Converse',
     imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-    // imageUrl: "/images/shop-img/sneakers/black-converse.png",
+    
     price: 110,
-    colors:['red','green','blue']
+    amount:1,
+    colors:['red','green','blue'],
+    size:['5','6']
 },
 {
     id: 13,
     name: 'Nike White AirForce',
     imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-    // imageUrl: "/images/shop-img/sneakers/white-nike-high-tops.png",
+    
     price: 160,
-    colors:['red','green','blue']
+    amount:1,
+    colors:['red','green','blue'],
+    size:['5','6']
 },
 {
     id: 14,
     name: 'Nike Red High Tops',
     imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-    // imageUrl: "/images/shop-img/sneakers/nikes-red.png",
+    
     price: 160,
-    colors:['red','green','blue']
+    amount:1,
+    colors:['red','green','blue'],
+    size:['5','6']
 },
 {
     id: 15,
     name: 'Nike Brown High Tops',
     imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-    // imageUrl: "/images/shop-img/sneakers/nike-brown.png",
+    
     price: 160,
-    colors:['red','green','blue']
+    amount:1,
+    colors:['red','green','blue'],
+    size:['5','6']
 },
 {
     id: 16,
     name: 'Air Jordan Limited',
     imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-    // imageUrl: "/images/shop-img/sneakers/nike-funky.png",
+    
     price: 190,
-    colors:['red','green','blue']
+    amount:1,
+    colors:['red','green','blue'],
+    size:['5','6']
 },
 {
     id: 17,
     name: 'Timberlands',
     imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-    // imageUrl: "/images/shop-img/sneakers/timberlands.png",
+    
     price: 200,
-    colors:['red','green','blue']
+    amount:1,
+    colors:['red','green','blue'],
+    size:['5','6']
 }
 ]
 export default data;
